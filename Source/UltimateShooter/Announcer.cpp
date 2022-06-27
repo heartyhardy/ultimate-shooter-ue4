961c465c0461f5b7dadebe20693a429670df88f4
heartyhardy <charith.n.rathnayake@gmail.com>
2022-06-28 00:43:44 +0530

    Added the basic implementation of the Male Announcer

---
+++ Source/UltimateShooter/Announcer.cpp
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Announcer.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
+
+// Sets default values
+AAnnouncer::AAnnouncer()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AAnnouncer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAnnouncer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AAnnouncer::PlayDamageBuffAnnouncement()
+{
+	if (DamageBuffAnnounce)
+	{
+		UGameplayStatics::PlaySound2D(
+			GetWorld(),
+			DamageBuffAnnounce
+		);
+	}
+}
+
+void AAnnouncer::PlayArmorBuffAnnouncement()
+{
+	if (ArmorBuffAnnounce)
+	{
+		UGameplayStatics::PlaySound2D(
+			GetWorld(),
+			ArmorBuffAnnounce
+		);
+	}
+}
+
+void AAnnouncer::PlaySpeedBuffAnnouncement()
+{
+	if (SpeedBuffAnnounce)
+	{
+		UGameplayStatics::PlaySound2D(
+			GetWorld(),
+			SpeedBuffAnnounce
+		);
+	}
+}
+
---
+++ Source/UltimateShooter/Announcer.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Announcer.generated.h"
+
+UCLASS()
+class ULTIMATESHOOTER_API AAnnouncer : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AAnnouncer();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, category = "Buffs", meta = (AllowPrivateAccess = "true"))
+		class USoundCue* DamageBuffAnnounce;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, category = "Buffs", meta = (AllowPrivateAccess = "true"))
+		class USoundCue* ArmorBuffAnnounce;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, category = "Buffs", meta = (AllowPrivateAccess = "true"))
+		class USoundCue* SpeedBuffAnnounce;
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+		void PlayDamageBuffAnnouncement();
+
+	UFUNCTION(BlueprintCallable)
+		void PlayArmorBuffAnnouncement();
+
+	UFUNCTION(BlueprintCallable)
+		void PlaySpeedBuffAnnouncement();
+};
